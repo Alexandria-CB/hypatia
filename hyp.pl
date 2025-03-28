c9dcf18df7b2687d4d0503b43b62bdd14bb617af
Alexandria Madeline Cina-Bernard <alexandriamc@debian-BULLSEYE-live-builder-AMD64>
2025-03-28 12:05:12 -0500

    improved argument handling

--- hyp.pl
+++ hyp.pl
@@ -3,4 +3,20 @@ use v5.14.0; # Compatible with Programming Perl 4th Edition
 use strict;
 use warnings;
 
-system("perl ./src/main.pl");
+sub help {
+    say "Usage:";
+    say "\tperl $0 <command> [options]";
+    say "Available Commands:";
+    say "\tbuild -> Assemble the static site.";
+}
+
+if (@ARGV == 0) {
+    help;
+}
+elsif (shift @ARGV eq "build") {
+    system("perl ./src/main.pl " . join(" ", @ARGV));
+}
+else {
+    say "Unrecognized command $ARGV[0]";
+    help;
+}
